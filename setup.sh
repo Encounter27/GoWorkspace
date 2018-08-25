b21fac0b599a84edf534c9185fec6268b95cbc7d
Pradip Acharjee <git config --global user.email>
2018-08-25 19:30:26 +0530

    Go workspace example

---
+++ setup.sh
@@ -0,0 +1,9 @@
+# Preacquisition:
+# Sytem already have go installed.
+
+# Install bellow packages:
+# "sudo apt-get install git direnv"
+
+
+# Add dependancies bellow
+go get -u -v github.com/derekparker/delve/cmd/dlv
